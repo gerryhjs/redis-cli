deb9f752f8b90189e44bdc999c50cf40f18cad44
Huang-JiaSheng <45413498+gerryhjs@users.noreply.github.com>
2021-07-23 11:41:06 +0800

    Update redis-cli.sh

--- redis-cli.sh
+++ redis-cli.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#ip="localhost"
-#port="6379"
-#password=""
-#ip="117.25.162.74"
-#port="26380"
-#password="starnet"
 
-ip="192.168.78.62"
-port="6380"
-password="starnet"
+ip=$1
+port=$2
+password=$3
 
-rm /tmp/allkeys.txt
-rm ./out.csv
-echo "keys '*'" | redis-cli -h "$ip" -p "$port" -a "$password" 2>/dev/null >/tmp/allkeys.txt
+file="redis@""$ip"":""$port"".csv"
+split="\""
+rm ./"$file"
+echo "keys '*'" | redis-cli -h "$ip" -p "$port" -a "$password" 2>/dev/null >./keys.txt
 
-echo "key;value" >>./out.csv
-for line in $(cat "/tmp/allkeys.txt"); do
-  echo -e "$line"";""\c" >>./out.csv
+echo "key,value,ttl" >>./"$file"
+for line in $(cat "./keys.txt"); do
+  echo -e "$split""$line""$split"",\c" >>./"$file"
   #  echo -e "     ""\c"
   value=$(echo "get $line" | redis-cli -h "$ip" -p "$port" -a "$password" 2>/dev/null)
-  value="${value//\"/\'}"
-  echo "\"""$value""\"">>./out.csv
-  #  echo -e "ttl:""\c"
-  #  echo "ttl $line" | redis-cli -h "$ip" -p "$port" -a "$password" 2>/dev/null
+  value="${value//$split/\"\"}"
+  echo -e "$split""$value""$split"",\c">>./"$file"
+  ttl=$(echo "ttl $line" | redis-cli -h "$ip" -p "$port" -a "$password" 2>/dev/null)
+  echo "$split""$ttl""    $split">>./"$file"
 done
+rm ./keys.txt
